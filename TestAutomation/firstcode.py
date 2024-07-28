304b276a21ec3bbddd90f891a82a8a2b24aed0c2
Siladitya Das <83537766+siladityadas@users.noreply.github.com>
2024-07-28 15:41:33 +0530

    This is our project initial state

--- TestAutomation/firstcode.py
+++ TestAutomation/firstcode.py
@@ -1,1 +1,13 @@
-print("print")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+driver = webdriver.Chrome()
+#="/Users/siladitya/Downloads/chromedriver-mac-arm64/chromedriver")
+driver.get("https://www.instagram.com/")
+driver.maximize_window()
+time.sleep(4)
+driver.find_element(By.XPATH,"//input[contains(@aria-label ,'username')]").send_keys("coolchintu.92@gmail.com")
+time.sleep(4)
+#frameObj = driver.find_element(By.XPATH,"//iframe[@id='backgroundImage']")
+#driver.switch_to.frame(frameObj)
+driver.execute_script("document.querySelector('body > ntp-app').shadowRoot.querySelector('#realbox').shadowRoot.querySelector('#input')")
